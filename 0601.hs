3602657ac43cd7dba411ad4754dd5ad269a5bd8d
motisn <motisn@motisn.localdomain>
2020-11-19 21:18:30 +0900

    Add error message

--- 0601.hs
+++ 0601.hs
@@ -1,13 +1,9 @@
 import Control.Monad.State
 --import Debug.Trace
 
-data T = Tint Int | Tabs T | Tapp T T | X deriving (Eq, Show)
-data DeBruijn = DeBruijn {
-    _context :: String,
-    _named :: String
-} deriving Show
-
-type Context = [String]
+-- 入力したラムダ式をパースしてASTに変換
+data AST = Var String | Abs String AST | App AST AST
+    deriving (Eq, Show)
 
 (<?-) :: (Eq a) => a -> [a] -> [Int]
 (<?-) a = filter (>=0) . zipWith (\i -> \x ->
@@ -27,9 +23,6 @@ tokenize = (\(ss, cs) ->
                     (ss, c:cs)
         )) ([], "")
 
-data AST = Var String | Abs String AST | App AST AST
-    deriving (Eq, Show)
-
 parse :: [String] -> AST
 parse ts = fst $ runState expr ts
 
@@ -40,7 +33,7 @@ expr = do
     t0 <- term
     ($ t0) . fix $ \loop t -> do
         token <- (state $ \(xs) -> case xs of
-            t:ts ->(t, ts)
+            t:ts -> (t, ts)
             [] -> ("", []))   -- pop的な
         case token of
             "" -> return t
@@ -55,22 +48,37 @@ term = do
     token <- (state $ \(t:ts) -> (t, ts))   -- pop的な
     case token of
         "(" -> do
-            -- 括弧の中身を独立して処理
-            residual <- (state $ \(ts) -> ($ (ts,[])) . ($ 1) . fix $ \loop i (t:ts,tekarb) ->
-                let i' = case t of
-                            "(" -> i + 1
-                            ")" -> i - 1
-                            _ -> i
-                in if i' == 0
-                        then (ts, reverse tekarb)
-                        else loop i' (ts,t:tekarb))
+            -- 括弧の中身をbraketに集めて独立して処理、残りをresidualに確保
+            residual <- (state $ \(ts) -> ($ (ts,[])) . ($ 1) . fix $ \loop i (xs,tekarb) -> case xs of
+                [] -> let residual = concat $ reverse tekarb   -- 残りの文字列を回収
+                    in error("Missing ')' of ...("++residual)
+                t:ts ->
+                    -- 多重括弧を除去
+                    let i' = case t of
+                                "(" -> i + 1
+                                ")" -> i - 1
+                                _ -> i
+                    in if i' == 0
+                            then (ts, reverse tekarb)
+                            else loop i' (ts,t:tekarb))
             expression <- expr
             state $ \(ts) -> ((), residual)
             return expression
-        ")" -> undefined
+        ")" -> do
+            residual <- (state $ \(ts) -> (concat ts, []))   -- 残りの文字列をpopで回収
+            error("Unexpexed ')' at ...)"++residual)
         _ -> if last token == '.'
             then do
                 expression <- expr
                 return $ Abs (init token) expression
             else do
                 return $ Var token
+
+-- ASTをde Bruijn項に変換
+data T = Tint Int | Tabs T | Tapp T T | X deriving (Eq, Show)
+data DeBruijn = DeBruijn {
+    _context :: String,
+    _named :: String
+} deriving Show
+
+type Context = [String]
